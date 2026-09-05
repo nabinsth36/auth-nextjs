639ae156a6b6b78b837957149d1fd75cb9ae8fd3
nabinsth36 <nabinsth@gmail.com>
2026-09-05 23:23:54 +0545

    authentication with nextjs

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="bg-amber-300 p-2 text-black m-2">Profile</h1>
+      <hr />
+      <p>Profile Page</p>
+    </div>
+  );
+}
